14007de771e18ffb8c0e51c0393ec0a4fee8ded8
Julia Holadava <juliaholadava@gmail.com>
2023-12-13 17:54:27 +0100

    feat: add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import Link from 'next/link';
+import router from 'next/router';
+import React, { useEffect, useState } from 'react';
+import LanguageSwitcher from './components';
+import Button from '../Button';
+import './Header.scss';
+
+const Header = () => {
+  const [isSticky, setSticky] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    setSticky(offset > 50);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={`header ${isSticky ? 'sticky' : ''}`}>
+      <Link className="header__logo" href="/">
+        GraphiQL
+      </Link>
+      <LanguageSwitcher />
+      <Button
+        type="button"
+        className="button"
+        text="Sign Out"
+        onClick={() => router.push('/')}
+      />
+    </header>
+  );
+};
+
+export default Header;
