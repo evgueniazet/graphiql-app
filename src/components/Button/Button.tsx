4dce6954f78b34eee0296738d6d553b63342ea4c
Julia Holadava <juliaholadava@gmail.com>
2023-12-13 16:57:28 +0100

    fix: update button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,13 +10,25 @@ interface IPropsButton {
   onClick?: () => void;
 }
 
-const Button = ({ type, className, children, text, onClick, disabled }: IPropsButton): ReactElement => {
+const Button = ({
+  type,
+  className,
+  children,
+  text,
+  onClick,
+  disabled,
+}: IPropsButton): ReactElement => {
   return (
-    <button type={type} className={className} onClick={onClick} disabled={disabled}>
+    <button
+      type={type}
+      className={className}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {text}
       {children}
     </button>
   );
-}
+};
 
 export default Button;
