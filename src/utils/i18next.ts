a33a3aaf678955e576cdce5e7fa45759977033a7
Julia Holadava <juliaholadava@gmail.com>
2023-12-13 13:05:23 +0100

    feat: setup i18next configuration

---
+++ src/utils/i18next.ts
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import translationEn from '../local/translationEn.json';
+import translationPl from '../local/translationPl.json';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: translationEn,
+    },
+    pl: {
+      translationPl,
+    },
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
