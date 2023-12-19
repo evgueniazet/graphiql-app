39a4f507265d5ad6094e441c2b6e436eb2b208e7
Evguenia Zialenka <zelenkoea19@gmail.com>
2023-12-19 15:25:47 +0100

    chore: remove some dependencies from the project

--- src/components/Header/components/LanguageSwitcher.tsx
+++ src/components/Header/components/LanguageSwitcher.tsx
@@ -1,20 +1,20 @@
 import React, { ChangeEvent } from 'react';
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 import styles from './LanguageSwitcher.module.scss';
 
 const LanguageSwitcher = () => {
-  const { i18n } = useTranslation();
+  //   const { i18n } = useTranslation();
 
   const handleChangeLanguage = (event: ChangeEvent<HTMLSelectElement>) => {
     const newLanguage = event.target.value;
-    i18n.changeLanguage(newLanguage);
+    // i18n.changeLanguage(newLanguage);
   };
 
   return (
     <select
       className={styles.switcher}
       onChange={handleChangeLanguage}
-      defaultValue={i18n.language}
+      //   defaultValue={i18n.language}
     >
       <option value="en">En</option>
       <option value="pl">Pl</option>
