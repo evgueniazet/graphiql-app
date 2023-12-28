4fa6079dbb9de888a6d7f689ad67d3397ca3a20e
Evguenia Zialenka <zelenkoea19@gmail.com>
2023-12-28 15:15:03 +0100

    feat: add EditorContext

---
+++ src/context/EditorContext.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface EditorContextProps {
+  children: ReactNode;
+}
+
+interface EditorContextValue {
+  editorValue: string;
+  handleEditorChange: (value: string) => void;
+}
+
+const EditorContext = createContext<EditorContextValue | undefined>(undefined);
+
+export const EditorProvider: React.FC<EditorContextProps> = ({ children }) => {
+  const [editorValue, setEditorValue] = useState(
+    "console.log('Monaco Editor!');"
+  );
+
+  const handleEditorChange = (value: string) => {
+    setEditorValue(value);
+  };
+
+  const contextValue: EditorContextValue = {
+    editorValue,
+    handleEditorChange,
+  };
+
+  return (
+    <EditorContext.Provider value={contextValue}>
+      {children}
+    </EditorContext.Provider>
+  );
+};
+
+export const useEditorContext = (): EditorContextValue => {
+  const context = useContext(EditorContext);
+  if (!context) {
+    throw new Error('useEditorContext must be used within an EditorProvider');
+  }
+  return context;
+};
