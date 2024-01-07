2c96cbddd738796a1a7d88a42c292f099215cd51
Julia Holadava <juliaholadava@gmail.com>
2024-01-07 20:42:18 +0100

    feat: update component to use AST schema

--- src/app/main/components/DocSection/DocSection.tsx
+++ src/app/main/components/DocSection/DocSection.tsx
@@ -1,26 +1,30 @@
+import { GraphQLSchema, isObjectType } from 'graphql';
 import styles from './DocSection.module.scss';
 import RenderFields from './RenderFields';
-import { GraphQLSchemaIntrospection } from './interfaces';
 
 type DocSectionProps = {
-  schemaData: GraphQLSchemaIntrospection;
+  schemaAST: GraphQLSchema;
 };
 
-const DocSection: React.FC<DocSectionProps> = ({ schemaData }) => {
-  const rootType = schemaData.__schema.queryType;
-  const rootFields =
-    schemaData.__schema.types.find((type) => type.name === rootType.name)
-      ?.fields || [];
+const DocSection: React.FC<DocSectionProps> = ({ schemaAST }) => {
+  const rootTypes = [
+    schemaAST.getQueryType(),
+    schemaAST.getMutationType(),
+    schemaAST.getSubscriptionType(),
+  ].filter(Boolean);
 
   return (
     <div className={styles.docSection_wrapper}>
       <h3 className={styles.heading}>Documentation</h3>
-      <h4 className={styles.heading5}>Root Types</h4>
-      <div>
-        <strong>query:</strong> {rootType.name}
-      </div>
-      <h4 className={styles.heading5}>Fields</h4>
-      <RenderFields fields={rootFields} schemaData={schemaData} />
+      {rootTypes.map(
+        (type) =>
+          type && (
+            <div key={type.name}>
+              <h4 className={styles.heading5}>{type.name} Type</h4>
+              {isObjectType(type) && <RenderFields fields={type.getFields()} />}
+            </div>
+          )
+      )}
     </div>
   );
 };
