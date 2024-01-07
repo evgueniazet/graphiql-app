d1414852ab32e7f090ec121309da34e3f3ffa404
Julia Holadava <juliaholadava@gmail.com>
2024-01-07 01:18:53 +0100

    feat: update component to render schema

--- src/app/main/components/DocSection/DocSection.tsx
+++ src/app/main/components/DocSection/DocSection.tsx
@@ -1,15 +1,26 @@
 import styles from './DocSection.module.scss';
-import RenderSchema from './RenderSchema';
+import RenderFields from './RenderFields';
+import { GraphQLSchemaIntrospection } from './interfaces';
 
-type DocSchemaProps<T> = {
-  schemaData: T;
+type DocSectionProps = {
+  schemaData: GraphQLSchemaIntrospection;
 };
 
-const DocSection = <T,>({ schemaData }: DocSchemaProps<T>) => {
+const DocSection: React.FC<DocSectionProps> = ({ schemaData }) => {
+  const rootType = schemaData.__schema.queryType;
+  const rootFields =
+    schemaData.__schema.types.find((type) => type.name === rootType.name)
+      ?.fields || [];
+
   return (
     <div className={styles.docSection_wrapper}>
       <h3 className={styles.heading}>Documentation</h3>
-      <RenderSchema data={schemaData} />
+      <h4 className={styles.heading5}>Root Types</h4>
+      <div>
+        <strong>query:</strong> {rootType.name}
+      </div>
+      <h4 className={styles.heading5}>Fields</h4>
+      <RenderFields fields={rootFields} schemaData={schemaData} />
     </div>
   );
 };
